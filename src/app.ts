d8dc2d7708859479ef7be2b961ab8beaf2c80815
Riunn <nassdev132@gmail.com>
2022-05-18 23:04:09 +0200

    Interacting with DOM Elements

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLFormElement
+    titleInputELement: HTMLInputElement
+    descriptionInputELement: HTMLInputElement
+    peopleInputELement: HTMLInputElement
 
     constructor(){
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
@@ -9,12 +12,29 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+
+        this.titleInputELement = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionInputELement = this.element.querySelector('#description') as HTMLInputElement
+        this.peopleInputELement = this.element.querySelector('#people') as HTMLInputElement
+
+        this.configure()
         this.attach()
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputELement.value)
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
     }
+
 }
 
 const prjInput = new ProjectInput()
