b5c547deef6807b925154bbb950123646615cbb3
Nassim Kissi <nassimkissi@icloud.com>
2022-05-19 10:15:18 +0200

    creating a re-usable validation functionality

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
+// Validation
+interface Validatable {
+    value: string | number
+    required?: boolean
+    minLength?: number
+    maxLength?: number
+    min?: number
+    max?: number
+}
+
+function validate(validatableInput: Validatable){
+    let isValid = true
+    if(validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0
+    }
+    if(validatableInput.minLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength
+    }
+    if(validatableInput.maxLength != null && typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength
+    }
+    if(validatableInput.min != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value >= validatableInput.min
+    }
+    if(validatableInput.max != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value <= validatableInput.max
+    }
+    return isValid
+}
+
 // autobbind decorator
 function autobbind(
     _: any,
@@ -44,13 +74,33 @@ class ProjectInput {
     private gatherUserInput(): [string, string, number] | void {
         const enteredTitle = this.titleInputELement.value
         const enteredDescription = this.descriptionInputELement.value
-        const enterPeople = this.peopleInputELement.value
+        const enteredPeople = this.peopleInputELement.value
 
-        if(enteredTitle.trim().length === 0 || enteredTitle.trim().length === 0 || enterPeople.trim().length === 0) {
+        const titleValidatable: Validatable = {
+            value: enteredTitle, 
+            required: true
+        }
+        const descriptionValidatable: Validatable = {
+            value: enteredDescription, 
+            required: true, 
+            minLength: 5
+        }
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople, 
+            required: true,
+            min: 1, 
+            max: 5
+        }
+
+        if(
+            !validate(titleValidatable) &&
+            !validate(descriptionValidatable) &&
+            !validate(peopleValidatable) 
+        ) {
             alert('Entrée invalide, veuillez rééssayer')
             return
         } else {
-            return [enteredTitle, enteredDescription, +enterPeople]
+            return [enteredTitle, enteredDescription, +enteredPeople]
         }
          
     }   
